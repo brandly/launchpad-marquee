5b681db4c7443a99922c7508c8cc145155f3dab9
brandly <brandly13@gmail.com>
2016-05-03 01:28:02 -0400

    init

---
+++ cli.js
@@ -0,0 +1,16 @@
+'use strict'
+const meow = require('meow')
+const launchpadMarquee = require('./')
+
+const cli = meow(`
+  Usage
+    $ foo <input>
+
+  Options
+    -r, --rainbow  Include a rainbow
+
+  Examples
+    $ launchpad-marquee "9876543210"
+`)
+
+launchpadMarquee(cli.input[0])
---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict'
+const MidiInput = require('midi').input
+const launchpadder = require('launchpadder')
+const Launchpad = launchpadder.Launchpad
+const marquee = require('./marquee')
+
+function findLaunchpadPort (input) {
+  for (var i = 0; i < input.getPortCount(); i++) {
+    if (input.getPortName(0) === 'Launchpad S') {
+      return i
+    }
+  }
+  return null
+}
+
+const input = new MidiInput()
+const midiInputCount = input.getPortCount()
+
+if (!midiInputCount) {
+  throw new Error('Unable to find MIDI input port')
+}
+
+const port = findLaunchpadPort(input)
+
+if (port === null) {
+  throw new Error('Unable to find Launchpad S')
+}
+
+const pad = new Launchpad(port)
+const color = launchpadder.Color.RED
+
+module.exports = function launchpadMarquee (text) {
+  return marquee(' ' + text, (marquee) => {
+    marquee.forEach((row, y) => {
+      row.forEach((light, x) => {
+        const button = pad.getButton(x, y)
+        light === 1 ? button.light(color) : button.dark()
+      })
+    })
+  })
+}
